82a6369a4a98c0f0f1efb23560e9a3a0cf59108d
ggomez30194 <54679440+ggomez30194@users.noreply.github.com>
2020-03-04 19:30:11 -0600

    1st Main Menu Implementation

---
+++ src/Menu.java
@@ -0,0 +1,62 @@
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.awt.image.BufferStrategy;
+import java.io.File;
+import java.nio.Buffer;
+import javax.swing.*;
+
+public class Menu extends JPanel {
+    public boolean started = false;
+
+    public String[] args;
+
+
+
+    public Menu(String[] arg) {
+        args = arg;
+        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+
+        add(Box.createVerticalStrut(30));
+
+        JLabel jlabel = new JLabel("TETRIS!");
+
+        jlabel.setFont(new Font("Algerian", Font.BOLD, 50));
+
+        jlabel.setForeground(Color.BLUE);
+        jlabel.setVerticalAlignment(jlabel.TOP);
+        jlabel.setAlignmentX(JPanel.CENTER_ALIGNMENT);
+
+
+
+        add(jlabel);
+
+        add(Box.createVerticalStrut(100));
+
+        Button playbutton = new Button("Play");
+        playbutton.setAlignmentX(JPanel.CENTER_ALIGNMENT);
+
+        playbutton.addActionListener(new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // create instance to play tetris
+            }
+        });
+        add(playbutton);
+}
+
+    @Override
+    public void paintComponent (Graphics g) {
+
+        super.paintComponent(g);
+        if (!started) {
+            g.drawImage(new ImageIcon("src/Tetris.jpg").getImage(), 0, 0, 720, 640, this);
+        } else {
+            setBackground(Color.RED);
+        }
+    }
+}
